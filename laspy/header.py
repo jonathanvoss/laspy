f5bb8e96c568636a09a0af000fc448419303ba6e
Jonathan Voss <jonathan.voss@uni.edu>
2015-12-22 21:30:57 -0600

    Remove an unused leap year function?

--- laspy/header.py
+++ laspy/header.py
@@ -10,17 +10,10 @@ import copy
 import numpy as np
 from laspy.compat import *
 
-def leap_year(year):
-    if ((year % 4) != 0):
-        return False
-    elif ((year % 100) != 0):
-        return True
-    elif ((year % 400) != 0):
-        return False
-    return True
 
 def pad(thing, with_thing, to_length):
     return thing + with_thing * (to_length - len(thing))
+
 ## NOTE: set_attr methods are currently not implemented. These methods need
 ## to update the file using reader/mmap.
 class LaspyHeaderException(Exception):
